f67fdb81f0d4b50e981da636062c187099035081
sean.fleming <sflem1993@gmail.com>
2017-08-08 02:45:54 -0400

    updating addpricedata to update a specific 'price_list' key in the map of info for each currency

--- src/server/data.js
+++ src/server/data.js
@@ -1,19 +1,19 @@
 import {List, Map} from 'immutable';
 
+export const DATA_POINTS = 'price_list';
 export const NULL_DATA_POINT = -1;
 export const INITIAL_STATE = Map();
-export const INITIAL_CURRENCY_STATE = List();
 export const MAX_SIZE = 5766; //storing data every 15 seconds for 24 hour period
 
 //currencyData - current list of data points for a specific currency
-//newCurrencyDataPoint - the new point of info being added to this currency
-export function addPriceData(currencyData = INITIAL_CURRENCY_STATE, newCurrencyDataPoint = NULL_DATA_POINT) {
-	if (currencyData.size < MAX_SIZE) {
-		return currencyData.push(newCurrencyDataPoint);
-	} else if (currencyData.size === MAX_SIZE) {
-		return currencyData.shift().push(newCurrencyDataPoint);
+//newDataPoint - the new point of info being added to this currency
+export function addPriceData(currencyData = INITIAL_STATE, newDataPoint = NULL_DATA_POINT) {
+	if (currencyData.get(DATA_POINTS).size < MAX_SIZE) {
+		return currencyData.update(DATA_POINTS, data_points => data_points.push(newDataPoint));
+	} else if (currencyData.get(DATA_POINTS).size === MAX_SIZE) {
+		return currencyData.update(DATA_POINTS, data_points => data_points.shift().push(newDataPoint));
 	} else {
-		return addPriceData(currencyData.shift(), newCurrencyDataPoint);
+		return addPriceData(currencyData.update(DATA_POINTS, data_points => data_points.shift()), newDataPoint);
 	}
 }
 
--- test/server/data.js
+++ test/server/data.js
@@ -7,48 +7,48 @@ import {addPriceData, updateCurrencies, MAX_SIZE} from '../../src/server/data';
 
 chai.use(chaiImmutable);
 
-describe('24 hour data for a coin', () => {
+describe('addPriceData', () => {
 	it('if less than max_size just add', () => {
-		const data = List.of(1, 2, 3);
+		const data = Map({'price_list': List.of(1, 2, 3)});
 		const updatedData = addPriceData(data, 4);
-		expect(updatedData).to.equal(List.of(1, 2, 3, 4));
+		expect(updatedData).to.equal(Map({'price_list': List.of(1, 2, 3, 4)}));
 	});
 
 	it('handles null new data', () => {
-		const data = List.of(1, 2, 3);
-		const updatedData = addPriceData(data, Map({a: 4}).get('c'));
-		expect(updatedData).to.equal(List.of(1, 2, 3, -1));
+		const data = Map({'price_list': List.of(1, 2, 3)});
+		const updatedData = addPriceData(data, Map({a: 4}).get('b'));
+		expect(updatedData).to.equal(Map({'price_list': List.of(1, 2, 3, -1)}));
 	});
 
 	it('if size = max_size, push to end and get rid of oldest value (at 0 index)', () => {
-		const data = Repeat(1, MAX_SIZE).toList();
+		const data = Map({'price_list': Repeat(1, MAX_SIZE).toList()});
 		const updatedData = addPriceData(data, 25);
-		expect(updatedData).to.equal(Repeat(1, MAX_SIZE - 1).toList().insert(MAX_SIZE - 1, 25));
+		expect(updatedData).to.equal(Map({
+			'price_list': Repeat(1, MAX_SIZE - 1).toList().insert(MAX_SIZE - 1, 25)
+		}));
 	});
 
 	it('if size > max_size, trims to max_size', () => {
-		const data = Repeat(1, MAX_SIZE + 100).toList();
+		const data = Map({'price_list': Repeat(1, MAX_SIZE + 100).toList()});
 		const updatedData = addPriceData(data, 25);
-		expect(updatedData).to.equal(Repeat(1, MAX_SIZE - 1).toList().insert(MAX_SIZE - 1, 25));
+		expect(updatedData).to.equal(Map({
+			'price_list': Repeat(1, MAX_SIZE - 1).toList().insert(MAX_SIZE - 1, 25)
+		}));
 	});
 });
 
-describe('updating each currency', () => {
+
+//testing our map method correctly hits the price_list for each value
+describe('updateCurrencies', () => {
 	it('updates each currency', () => {
-		const data = Map({a: List.of(4), b: List.of(5)});
+		const data = Map({
+			a: Map({'price_list': List.of(4)}),
+			b: Map({'price_list': List.of(5)})
+		});
 		const updatedData = updateCurrencies(data, Map({a: 6, b: 9}));
-		expect(updatedData).to.equal(Map({a: List.of(4, 6), b: List.of(5, 9)}));
-	});
-
-	it('handles no new data', () => {
-		const data = Map({a: List.of(4), b: List.of(5)});
-		const updatedData = updateCurrencies(data, undefined);
-		expect(updatedData).to.equal(Map({a: List.of(4, -1), b: List.of(5, -1)}));
-	});
-
-	it('handles no data for a currency', () => {
-		const data = Map({a: List.of(4), b: List.of(5)});
-		const updatedData = updateCurrencies(data, Map({a: 6}));
-		expect(updatedData).to.equal(Map({a: List.of(4, 6), b: List.of(5, -1)}));
+		expect(updatedData).to.equal(Map({
+			a: Map({'price_list': List.of(4, 6)}),
+			b: Map({'price_list': List.of(5, 9)})
+		}));
 	});
 });
