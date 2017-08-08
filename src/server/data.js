07f5c48cfc9d094a59c6bf3689acb50eca371e8e
sean.fleming <sflem1993@gmail.com>
2017-08-08 01:44:22 -0400

    writing methods for storing crypto info as a map of all currencies to their info && fixing start script

--- src/server/data.js
+++ src/server/data.js
@@ -1,14 +1,24 @@
 import {List, Map} from 'immutable';
 
-export const INITIAL_DATA = List();
-export const MAX_SIZE = 5760; //storing data every 15 seconds for 24 hour period
+export const NULL_DATA_POINT = -1;
+export const INITIAL_STATE = Map();
+export const INITIAL_CURRENCY_STATE = List();
+export const MAX_SIZE = 5766; //storing data every 15 seconds for 24 hour period
 
-export function addPriceData(data, newData) {
-	if (data.size < MAX_SIZE) {
-		return data.push(newData);
-	} else if (data.size === MAX_SIZE) {
-		return data.delete(MAX_SIZE - 1).insert(0, newData);
+//currencyData - current list of data points for a specific currency
+//newCurrencyDataPoint - the new point of info being added to this currency
+export function addPriceData(currencyData = INITIAL_CURRENCY_STATE, newCurrencyDataPoint = NULL_DATA_POINT) {
+	if (currencyData.size < MAX_SIZE) {
+		return currencyData.push(newCurrencyDataPoint);
+	} else if (currencyData.size === MAX_SIZE) {
+		return currencyData.shift().push(newCurrencyDataPoint);
 	} else {
-		return addPriceData(data.delete(data.size - 1), newData);
+		return addPriceData(currencyData.shift(), newCurrencyDataPoint);
 	}
+}
+
+//data - current data, aka map of all currencies to info
+//newCurrencyData - map of all currencies to new data
+export function updateCurrencies(data = INITIAL_STATE, newCurrencyData = INITIAL_STATE) {
+	return data.map((currencyData, currency) => addPriceData(currencyData, newCurrencyData.get(currency)));
 }
--- test/server/data.js
+++ test/server/data.js
@@ -3,7 +3,7 @@ import Bittrex from 'bittrex-wrapper';
 import {List, Map, Repeat} from 'immutable';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
-import {addPriceData, MAX_SIZE} from '../../src/server/data';
+import {addPriceData, updateCurrencies, MAX_SIZE} from '../../src/server/data';
 
 chai.use(chaiImmutable);
 
@@ -14,16 +14,41 @@ describe('24 hour data for a coin', () => {
 		expect(updatedData).to.equal(List.of(1, 2, 3, 4));
 	});
 
-	it('if size = max_size, push to front and pop latest value', () => {
+	it('handles null new data', () => {
+		const data = List.of(1, 2, 3);
+		const updatedData = addPriceData(data, Map({a: 4}).get('c'));
+		expect(updatedData).to.equal(List.of(1, 2, 3, -1));
+	});
+
+	it('if size = max_size, push to end and get rid of oldest value (at 0 index)', () => {
 		const data = Repeat(1, MAX_SIZE).toList();
 		const updatedData = addPriceData(data, 25);
-		expect(updatedData).to.equal(Repeat(1, MAX_SIZE - 1).toList().insert(0, 25));
-
+		expect(updatedData).to.equal(Repeat(1, MAX_SIZE - 1).toList().insert(MAX_SIZE - 1, 25));
 	});
 
 	it('if size > max_size, trims to max_size', () => {
 		const data = Repeat(1, MAX_SIZE + 100).toList();
 		const updatedData = addPriceData(data, 25);
-		expect(updatedData).to.equal(Repeat(1, MAX_SIZE - 1).toList().insert(0, 25));
+		expect(updatedData).to.equal(Repeat(1, MAX_SIZE - 1).toList().insert(MAX_SIZE - 1, 25));
+	});
+});
+
+describe('updating each currency', () => {
+	it('updates each currency', () => {
+		const data = Map({a: List.of(4), b: List.of(5)});
+		const updatedData = updateCurrencies(data, Map({a: 6, b: 9}));
+		expect(updatedData).to.equal(Map({a: List.of(4, 6), b: List.of(5, 9)}));
+	});
+
+	it('handles no new data', () => {
+		const data = Map({a: List.of(4), b: List.of(5)});
+		const updatedData = updateCurrencies(data, undefined);
+		expect(updatedData).to.equal(Map({a: List.of(4, -1), b: List.of(5, -1)}));
+	});
+
+	it('handles no data for a currency', () => {
+		const data = Map({a: List.of(4), b: List.of(5)});
+		const updatedData = updateCurrencies(data, Map({a: 6}));
+		expect(updatedData).to.equal(Map({a: List.of(4, 6), b: List.of(5, -1)}));
 	});
 });
