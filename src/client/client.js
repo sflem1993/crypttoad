dfdfdc4518a002623de64ceca9dfb23f7a39c1e9
sean.fleming <sflem1993@gmail.com>
2017-08-13 17:43:59 -0400

    initial react/babel/webpack client config

--- src/client/client.js
+++ src/client/client.js
@@ -1,3 +1,6 @@
 import io from 'socket.io-client';
+import {getBittrexInfo} from './../server/data';
+
+import {Map} from 'immutable';
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ module.exports = {
 	entry: [
 		'./src/client/client.js'
 	],
+	module: {
+		loaders: [{
+	    	//test: /\.jsk?$/,
+    		test: /\.(js|jsx)$/,
+    		exclude: /node_modules/,
+      		loader: 'babel-loader',
+      		query: {
+      			presets: ['react', 'es2015']
+      		}
+		}]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
 	output: {
 		path: __dirname + '/dist',
 		publicPath: '/',
