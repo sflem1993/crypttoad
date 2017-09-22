eddb78d9f23688397e46d94e82d9e824f956476f
sean.fleming <sflem1993@gmail.com>
2017-09-21 22:37:09 -0400

    redoing

--- src/client/components/Crypttoad.jsx
+++ src/client/components/Crypttoad.jsx
@@ -3,16 +3,40 @@ import MarketSelect from './MarketSelect';
 import MainPage from './MainPage';
 import AutoSelect from './AutoSelect';
 
+import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Area} from 'recharts';
+
+
 
 import logo from './../img/rotaty.gif';
 
 export default class Crypttoad extends React.PureComponent {
+	getData() {
+		return this.props.data || [];
+	}
 	render() {
 		return <div className="crypttoad">
 			<div className="leftSide">
 				<div className="logo"><img src={logo}/></div>
 				<MarketSelect {...this.props}/>
 			</div>
+			<div className="mainPageInfo">
+				<div className="currencySelect"><AutoSelect/></div>
+				<div className="markets">B</div>
+			{/*
+				<div className="currencySelect"><AutoSelect/></div>
+				<div className="markets">
+					<ResponsiveContainer height="100%" width="100%">
+						    		<LineChart data={this.getData()}>
+									  	<XAxis dataKey="name"/>
+								        <YAxis />
+								        <CartesianGrid strokeDashArray="3 3" vertical={false}/>
+								        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+							      	</LineChart>
+					</ResponsiveContainer>
+				</div>
+			*/}
+			</div>
+			{/*
 			<div className="theStuff">
 				<div className="topStuff">
 					<div className="theSelect"><AutoSelect/></div>
@@ -25,6 +49,7 @@ export default class Crypttoad extends React.PureComponent {
 					<div>C FOOT</div>
 				</div>
 			</div>
+		*/}
 		</div>
 	}
 }
