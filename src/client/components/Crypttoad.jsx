1715751046f32cd5c39e8ab9209c020a0739610e
sean.fleming <sflem1993@gmail.com>
2017-10-27 02:27:00 -0400

    for shuffling sidebar

--- src/client/components/Crypttoad.jsx
+++ src/client/components/Crypttoad.jsx
@@ -15,115 +15,102 @@ export default class Crypttoad extends React.PureComponent {
 	}
 	render() {
 		return <div className="crypttoad">
-			<div className="leftSide">
-				<div className="logo"><img src={logo}/></div>
-				<MarketSelect {...this.props}/>
-			</div>
-			<div className="mainPageInfo">
-				<div className="top">
-					<div className="currencySelect"><AutoSelect/></div>
+				<div className="main">
+				<div className="leftSide">
+					<div className="logo"><img src={logo}/></div>
+					<MarketSelect {...this.props}/>
 				</div>
-				<div className="markets">
-					<div className="market">
-						<div className="marketGraph">
-							<ResponsiveContainer height="100%" width="100%">
-						    		<LineChart data={this.getData()}>
-									  	<XAxis strokeWidth={2} dataKey="name"/>
-								        <YAxis strokeWidth={2} />
-								        <CartesianGrid  strokeDashArray="3 3" vertical={false}/>
-								        <Line strokeWidth={5} type="monotone" dataKey="uv" stroke="#e3e3e3" />
-							      	</LineChart>
-							</ResponsiveContainer>
-						</div>
-
-						<div className="marketStats">
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">Current Price:</div>
-								<div className="marketStatsRowValue">0.00000904</div>
-							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">Current Bid:</div>
-								<div className="marketStatsRowValue">0.00000890</div>
-							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">Current Ask:</div>
-								<div className="marketStatsRowValue">0.00000900</div>
-							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">24 Hour High:</div>
-								<div className="marketStatsRowValue">0.00000919</div>
-							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">24 Hour Low:</div>
-								<div className="marketStatsRowValue">0.00000819</div>
-							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">24 Hours Ago:</div>
-								<div className="marketStatsRowValue">0.00000900</div>
-							</div>
-						</div>
+				<div className="mainPageInfo">
+					<div className="top">
+						<div className="currencySelect"><AutoSelect/></div>
 					</div>
+					<div className="markets">
 						<div className="market">
-						<div className="marketGraph">
-							<ResponsiveContainer height="100%" width="100%">
-						    		<LineChart data={this.getData()}>
-									  	<XAxis strokeWidth={2} dataKey="name"/>
-								        <YAxis strokeWidth={2} />
-								        <CartesianGrid  strokeDashArray="3 3" vertical={false}/>
-								        <Line strokeWidth={5} type="monotone" dataKey="uv" stroke="#e3e3e3" />
-							      	</LineChart>
-							</ResponsiveContainer>
-						</div>
+							<div className="marketGraph">
+								<ResponsiveContainer height="100%" width="100%">
+							    		<LineChart data={this.getData()}>
+										  	<XAxis strokeWidth={2} dataKey="name"/>
+									        <YAxis strokeWidth={2} />
+									        <CartesianGrid  strokeDashArray="3 3" vertical={false}/>
+									        <Line strokeWidth={5} type="monotone" dataKey="uv" stroke="#e3e3e3" />
+								      	</LineChart>
+								</ResponsiveContainer>
+							</div>
 
-						<div className="marketStats">
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">Current Price:</div>
-								<div className="marketStatsRowValue">0.00000904</div>
+							<div className="marketStats">
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">Current Price:</div>
+									<div className="marketStatsRowValue">0.00000904</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">Current Bid:</div>
+									<div className="marketStatsRowValue">0.00000890</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">Current Ask:</div>
+									<div className="marketStatsRowValue">0.00000900</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">24 Hour High:</div>
+									<div className="marketStatsRowValue">0.00000919</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">24 Hour Low:</div>
+									<div className="marketStatsRowValue">0.00000819</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">24 Hours Ago:</div>
+									<div className="marketStatsRowValue">0.00000900</div>
+								</div>
 							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">Current Bid:</div>
-								<div className="marketStatsRowValue">0.00000890</div>
+						</div>
+							<div className="market">
+							<div className="marketGraph">
+								<ResponsiveContainer height="100%" width="100%">
+							    		<LineChart data={this.getData()}>
+										  	<XAxis strokeWidth={2} dataKey="name"/>
+									        <YAxis strokeWidth={2} />
+									        <CartesianGrid  strokeDashArray="3 3" vertical={false}/>
+									        <Line strokeWidth={5} type="monotone" dataKey="uv" stroke="#e3e3e3" />
+								      	</LineChart>
+								</ResponsiveContainer>
 							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">Current Ask:</div>
-								<div className="marketStatsRowValue">0.00000900</div>
-							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">24 Hour High:</div>
-								<div className="marketStatsRowValue">0.00000919</div>
-							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">24 Hour Low:</div>
-								<div className="marketStatsRowValue">0.00000819</div>
-							</div>
-							<div className="marketStatsRow">
-								<div className="marketStatsRowLabel">24 Hours Ago:</div>
-								<div className="marketStatsRowValue">0.00000900</div>
+
+							<div className="marketStats">
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">Current Price:</div>
+									<div className="marketStatsRowValue">0.00000904</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">Current Bid:</div>
+									<div className="marketStatsRowValue">0.00000890</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">Current Ask:</div>
+									<div className="marketStatsRowValue">0.00000900</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">24 Hour High:</div>
+									<div className="marketStatsRowValue">0.00000919</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">24 Hour Low:</div>
+									<div className="marketStatsRowValue">0.00000819</div>
+								</div>
+								<div className="marketStatsRow">
+									<div className="marketStatsRowLabel">24 Hours Ago:</div>
+									<div className="marketStatsRowValue">0.00000900</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className="footer">
-
+				<div className="rightSidebar">
+					SS
 				</div>
 			</div>
-			<div className="rightSidebar">
-				SS
+			<div className="footer">
 			</div>
-			{/*
-			<div className="theStuff">
-				<div className="topStuff">
-					<div className="theSelect"><AutoSelect/></div>
-					<div className="topRight">RIGHT TOP</div>
-				</div>
-				<MainPage {...this.props}/>
-				<div className="footer">
-					<div>A FOOT</div>
-					<div>B FOOT</div>
-					<div>C FOOT</div>
-				</div>
-			</div>
-		*/}
 		</div>
 	}
 }
