4051e698bd4ff21d298fb10f4aa15a4fb82bd2db
sean.fleming <sflem1993@gmail.com>
2017-10-27 02:04:40 -0400

    sidebar

--- src/client/components/Crypttoad.jsx
+++ src/client/components/Crypttoad.jsx
@@ -20,9 +20,50 @@ export default class Crypttoad extends React.PureComponent {
 				<MarketSelect {...this.props}/>
 			</div>
 			<div className="mainPageInfo">
-				<div className="currencySelect"><AutoSelect/></div>
+				<div className="top">
+					<div className="currencySelect"><AutoSelect/></div>
+				</div>
 				<div className="markets">
 					<div className="market">
+						<div className="marketGraph">
+							<ResponsiveContainer height="100%" width="100%">
+						    		<LineChart data={this.getData()}>
+									  	<XAxis strokeWidth={2} dataKey="name"/>
+								        <YAxis strokeWidth={2} />
+								        <CartesianGrid  strokeDashArray="3 3" vertical={false}/>
+								        <Line strokeWidth={5} type="monotone" dataKey="uv" stroke="#e3e3e3" />
+							      	</LineChart>
+							</ResponsiveContainer>
+						</div>
+
+						<div className="marketStats">
+							<div className="marketStatsRow">
+								<div className="marketStatsRowLabel">Current Price:</div>
+								<div className="marketStatsRowValue">0.00000904</div>
+							</div>
+							<div className="marketStatsRow">
+								<div className="marketStatsRowLabel">Current Bid:</div>
+								<div className="marketStatsRowValue">0.00000890</div>
+							</div>
+							<div className="marketStatsRow">
+								<div className="marketStatsRowLabel">Current Ask:</div>
+								<div className="marketStatsRowValue">0.00000900</div>
+							</div>
+							<div className="marketStatsRow">
+								<div className="marketStatsRowLabel">24 Hour High:</div>
+								<div className="marketStatsRowValue">0.00000919</div>
+							</div>
+							<div className="marketStatsRow">
+								<div className="marketStatsRowLabel">24 Hour Low:</div>
+								<div className="marketStatsRowValue">0.00000819</div>
+							</div>
+							<div className="marketStatsRow">
+								<div className="marketStatsRowLabel">24 Hours Ago:</div>
+								<div className="marketStatsRowValue">0.00000900</div>
+							</div>
+						</div>
+					</div>
+						<div className="market">
 						<div className="marketGraph">
 							<ResponsiveContainer height="100%" width="100%">
 						    		<LineChart data={this.getData()}>
