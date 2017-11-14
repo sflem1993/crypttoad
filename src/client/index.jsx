43c9c6f18316a5581b45652695cc865fe0280091
sean.fleming <sflem1993@gmail.com>
2017-11-13 22:37:06 -0500

    removing log used for debugging

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -15,7 +15,6 @@ const store = createStore(reducer);
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 
 socket.on('state', state => {
-	console.log(state);
 	store.dispatch(setState(state));
 });
 
